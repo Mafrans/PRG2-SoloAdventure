c5e404abe400564930735263f3dfe0e0a49431d3
Mafrans <malte.kluft@gmail.com>
2021-02-02 13:32:07 +0100

    Skapa en grundskiss för fiende-editorn

---
+++ src/main/kotlin/me/mafrans/soloadventure/editor/EnemyEditor.java
@@ -0,0 +1,15 @@
+package me.mafrans.soloadventure.editor;
+
+import javax.swing.*;
+
+public class EnemyEditor {
+    private JTextField nameField;
+    private JSpinner hpSpinner;
+    private JButton editSpriteButton;
+    private JPanel mainPanel;
+    private JPanel spritePanel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
