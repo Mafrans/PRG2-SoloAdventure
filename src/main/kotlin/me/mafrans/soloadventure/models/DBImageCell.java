a403db95a08d1115a38e4984d9fd32de63d5217a
Mafrans <malte.kluft@gmail.com>
2021-02-02 13:25:53 +0100

    Lägg till DBImageCell-modellen

---
+++ src/main/kotlin/me/mafrans/soloadventure/models/DBImageCell.java
@@ -0,0 +1,11 @@
+package me.mafrans.soloadventure.models;
+
+public class DBImageCell {
+    public String text;
+    public DBColorStyle style;
+
+    public DBImageCell(String text, DBColorStyle style) {
+        this.text = text;
+        this.style = style;
+    }
+}
