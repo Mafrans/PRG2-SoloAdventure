d012a9fe7218836befe528bbfecc2ab2e9f9bfd2
Mafrans <malte.kluft@gmail.com>
2021-02-02 12:38:46 +0100

    Sätt färgerna i en grid med 4 rader

--- src/main/kotlin/me/mafrans/soloadventure/editor/ColorPickerPanel.kt
+++ src/main/kotlin/me/mafrans/soloadventure/editor/ColorPickerPanel.kt
@@ -2,7 +2,7 @@ package me.mafrans.soloadventure.editor
 
 import java.awt.Color
 import java.awt.Dimension
-import java.awt.GridBagLayout
+import java.awt.GridLayout
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.BorderFactory
@@ -10,12 +10,11 @@ import javax.swing.JPanel
 
 class ColorPickerPanel(painter: AsciiEditorPainter, colors: Array<Color>) : JPanel() {
     init {
-        layout = GridBagLayout()
+        layout = GridLayout(4, 0, 5, 5)
 
         for (color in colors) {
             val panel = ColoredPanel(color)
             panel.preferredSize = Dimension(20, 20)
-            panel.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
             panel.addMouseListener(object : MouseAdapter() {
                 override fun mouseClicked(e: MouseEvent?) {
                     painter.currentColor = color
