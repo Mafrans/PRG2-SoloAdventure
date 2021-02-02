05d9e2ce7a9a403cd2759bd8ee9dca4f142b3276
Mafrans <malte.kluft@gmail.com>
2021-02-02 13:20:11 +0100

    Gör så man kan sätta både background och foreground

--- src/main/kotlin/me/mafrans/soloadventure/editor/AsciiEditorPainter.kt
+++ src/main/kotlin/me/mafrans/soloadventure/editor/AsciiEditorPainter.kt
@@ -10,9 +10,17 @@ import javax.swing.JPanel
 import javax.swing.event.TableModelEvent
 import javax.swing.event.TableModelListener
 
+class CellColor(var background: AsciiColor?, var foreground: AsciiColor?)
+
+enum class DrawType {
+    FOREGROUND,
+    BACKGROUND
+}
+
 class AsciiEditorPainter(val table: AsciiEditorTable) : JPanel() {
-    val colors: Array<Array<Color?>> = Array(table.columns) { Array(table.rows) { null } }
-    var currentColor = Color.RED
+    val colors: Array<Array<CellColor>> = Array(table.columns) { Array(table.rows) { CellColor(null, null) } }
+    var currentColor = AsciiColor.WHITE
+    var drawType: DrawType = DrawType.FOREGROUND;
     init {
         preferredSize = Dimension(table.preferredSize.height, table.preferredSize.height)
         addMouseListener(object : MouseAdapter() {
@@ -21,7 +29,11 @@ class AsciiEditorPainter(val table: AsciiEditorTable) : JPanel() {
                 val cellX = e.x / (width / colors.size)
                 val cellY = e.y / (height / colors[0].size)
 
-                colors[cellX][cellY] = currentColor
+                when(drawType) {
+                    DrawType.FOREGROUND -> colors[cellX][cellY].foreground = currentColor
+                    DrawType.BACKGROUND -> colors[cellX][cellY].background = currentColor
+                }
+
                 repaint()
             }
         })
@@ -37,26 +49,27 @@ class AsciiEditorPainter(val table: AsciiEditorTable) : JPanel() {
                 val cellX = x * cellWidth
                 val cellY = y * cellHeight
 
-                if (colors[x][y] == null)
-                    g?.color = Color.WHITE
+                val background = colors[x][y].background
+                if (background == null)
+                    g?.color = Color.BLACK
                 else
-                    g?.color = colors[x][y];
+                    g?.color = background.color;
 
                 g?.fillRect(cellX, cellY, cellWidth, cellHeight)
 
-                val saturation = (g?.color!!.red + g.color!!.green + g.color!!.blue) / (3.0 * 256)
-                println(saturation)
-                g.color = Color.BLACK
-                if (saturation < 0.5)
-                    g.color = Color.WHITE
+                val foreground = colors[x][y].foreground
+                if (foreground == null)
+                    g?.color = Color.WHITE
+                else
+                    g?.color = foreground.color;
 
                 val text: String? = table.model.getValueAt(y, x) as String?
                 if (text != null) {
-                    g.drawString(text, cellX + cellWidth/2 - 4, cellY + cellHeight/2 + 4)
+                    g?.drawString(text, cellX + cellWidth/2 - 4, cellY + cellHeight/2 + 4)
                 }
 
-                g.color = Color.BLACK
-                g.drawRect(cellX, cellY, cellWidth, cellHeight)
+                g?.color = Color.WHITE
+                g?.drawRect(cellX, cellY, cellWidth, cellHeight)
             }
         }
     }
