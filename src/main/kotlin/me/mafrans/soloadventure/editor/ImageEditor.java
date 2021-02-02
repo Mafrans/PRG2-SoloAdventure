d8c080c38493dc1a0e14c7b8c025b11484124419
Mafrans <malte.kluft@gmail.com>
2021-02-02 13:20:30 +0100

    Lägg till det nya systemet i ImageEditor.form

--- src/main/kotlin/me/mafrans/soloadventure/editor/ImageEditor.java
+++ src/main/kotlin/me/mafrans/soloadventure/editor/ImageEditor.java
@@ -1,23 +1,26 @@
 package me.mafrans.soloadventure.editor;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class ImageEditor {
     private AsciiEditorTable asciiEditorTable;
     private JPanel mainPanel;
     private JPanel asciiEditorPainterWrapper;
     private JPanel colorPickerWrapper;
+    private JRadioButton foregroundRadioButton;
+    private JRadioButton backgroundRadioButton;
+
+    public AsciiEditorPainter asciiEditorPainter;
 
     private void createUIComponents() {
         asciiEditorTable = new AsciiEditorTable(10, 10);
 
-        AsciiEditorPainter painter = new AsciiEditorPainter(asciiEditorTable);
+        asciiEditorPainter = new AsciiEditorPainter(asciiEditorTable);
         asciiEditorPainterWrapper = new JPanel();
-        asciiEditorPainterWrapper.add(painter);
+        asciiEditorPainterWrapper.add(asciiEditorPainter);
 
         colorPickerWrapper = new JPanel();
-        colorPickerWrapper.add(new ColorPickerPanel(painter, new Color[] { Color.RED, Color.GREEN, Color.BLUE }));
+        colorPickerWrapper.add(new ColorPickerPanel(asciiEditorPainter, AsciiColor.values()));
     }
 
     public ImageEditor() {
@@ -26,5 +29,13 @@ public class ImageEditor {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+        foregroundRadioButton.addActionListener(e -> {
+            asciiEditorPainter.setDrawType(DrawType.FOREGROUND);
+        });
+
+        backgroundRadioButton.addActionListener(e -> {
+            asciiEditorPainter.setDrawType(DrawType.BACKGROUND);
+        });
     }
 }
