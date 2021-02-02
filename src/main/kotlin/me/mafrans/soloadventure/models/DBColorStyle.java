22be8fa3b73412f3ba08ad629d0a6d01e0d487b6
Mafrans <malte.kluft@gmail.com>
2021-02-02 13:23:49 +0100

    Lägg till DBColorStyle-modellen

---
+++ src/main/kotlin/me/mafrans/soloadventure/models/DBColorStyle.java
@@ -0,0 +1,11 @@
+package me.mafrans.soloadventure.models;
+
+public class DBColorStyle {
+    public int foreground;
+    public int background;
+
+    public DBColorStyle(int foreground, int background) {
+        this.foreground = foreground;
+        this.background = background;
+    }
+}
