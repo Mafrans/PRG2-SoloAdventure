ac22508bfd7cbf47198e8b81e887922b35371ace
Mafrans <malte.kluft@gmail.com>
2021-02-02 13:19:51 +0100

    Använd AsciiColor istället för vanliga Color

--- src/main/kotlin/me/mafrans/soloadventure/editor/ColorPickerPanel.kt
+++ src/main/kotlin/me/mafrans/soloadventure/editor/ColorPickerPanel.kt
@@ -8,12 +8,12 @@ import java.awt.event.MouseEvent
 import javax.swing.BorderFactory
 import javax.swing.JPanel
 
-class ColorPickerPanel(painter: AsciiEditorPainter, colors: Array<Color>) : JPanel() {
+class ColorPickerPanel(painter: AsciiEditorPainter, colors: Array<AsciiColor>) : JPanel() {
     init {
         layout = GridLayout(4, 0, 5, 5)
 
         for (color in colors) {
-            val panel = ColoredPanel(color)
+            val panel = ColoredPanel(color.color)
             panel.preferredSize = Dimension(20, 20)
             panel.addMouseListener(object : MouseAdapter() {
                 override fun mouseClicked(e: MouseEvent?) {
