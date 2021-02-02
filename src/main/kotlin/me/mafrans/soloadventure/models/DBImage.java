4ab7739f0ac0537b01fe9c69890270cf51c4176c
Mafrans <malte.kluft@gmail.com>
2021-02-02 13:26:39 +0100

    Lägg till DBImage-modellen

---
+++ src/main/kotlin/me/mafrans/soloadventure/models/DBImage.java
@@ -0,0 +1,9 @@
+package me.mafrans.soloadventure.models;
+
+public class DBImage {
+    public DBImageCell[][] cells;
+
+    public DBImage(DBImageCell[][] cells) {
+        this.cells = cells;
+    }
+}
