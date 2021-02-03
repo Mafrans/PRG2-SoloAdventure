ce8f27b72126ceb683a988c74fdfb85d9c1167ed
Mafrans <malte.kluft@gmail.com>
2021-02-03 12:42:10 +0100

    Gör fiender sökbara

--- src/main/kotlin/me/mafrans/soloadventure/Database.kt
+++ src/main/kotlin/me/mafrans/soloadventure/Database.kt
@@ -4,20 +4,36 @@ import com.mongodb.client.MongoClients
 import dev.morphia.Datastore
 import dev.morphia.Morphia
 import dev.morphia.query.experimental.filters.Filters
+import me.mafrans.soloadventure.models.DBEnemy
 import me.mafrans.soloadventure.models.DBImage
 
 class Database {
+    private val datastore: Datastore = Morphia.createDatastore(MongoClients.create(), "soloadventure")
+    init {
+        datastore.mapper.mapPackage("me.mafrans.soloadventure.models")
+        datastore.ensureIndexes()
+    }
+
+    companion object {
+        val db = Database()
+    }
+
     class Images {
         companion object {
-            private val datastore: Datastore = Morphia.createDatastore(MongoClients.create(), "images")
-
-            init {
-                datastore.mapper.mapPackage("me.mafrans.soloadventure.models")
-                datastore.ensureIndexes()
-            }
-
             fun findImage(name: String): DBImage? {
-                val images = this.datastore.find(DBImage::class.java).filter(Filters.gte("name", name))
+                val images = db.datastore.find(DBImage::class.java).filter(Filters.gte("name", name))
+                if (images.count() == 0L)
+                    return null
+                else
+                    return images.first()
+            }
+        }
+    }
+
+    class Enemies {
+        companion object {
+            fun findEnemy(name: String): DBEnemy? {
+                val images = db.datastore.find(DBEnemy::class.java).filter(Filters.gte("name", name))
                 if (images.count() == 0L)
                     return null
                 else
---
+++ src/main/kotlin/me/mafrans/soloadventure/models/DBEnemy.java
@@ -0,0 +1,24 @@
+package me.mafrans.soloadventure.models;
+
+import me.mafrans.soloadventure.Database;
+
+public class DBEnemy {
+    public String name;
+    public int hp;
+    public DBImage sprite;
+
+    public DBEnemy(String name, int hp, DBImage sprite) {
+        this.name = name;
+        this.hp = hp;
+        this.sprite = sprite;
+    }
+
+    public DBEnemy(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+
+    public static DBEnemy find(String name) {
+        return Database.Enemies.Companion.findEnemy(name);
+    }
+}
