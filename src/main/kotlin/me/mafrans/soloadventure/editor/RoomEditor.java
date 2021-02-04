9a7365217a56ee71db17ef21b04fb74c15b21f5d
Mafrans <malte.kluft@gmail.com>
2021-02-04 19:27:24 +0100

    Skapa en grundläggande RoomEditor design

---
+++ src/main/kotlin/me/mafrans/soloadventure/editor/RoomEditor.java
@@ -0,0 +1,19 @@
+package me.mafrans.soloadventure.editor;
+
+import javax.swing.*;
+
+public class RoomEditor {
+    private JTabbedPane tabbedPane1;
+    private JPanel mainPanel;
+    private JComboBox roomColorComboBox;
+    private JTextArea roomTextArea;
+    private JButton editImageButton;
+    private JButton addEnemyButton;
+    private JButton addItemButton;
+    private JButton saveButton;
+    private JButton addInspectionButton;
+    private JPanel itemContainer;
+    private JPanel enemyContainer;
+    private JPanel inspectionContainer;
+    private JPanel roomImageWrapper;
+}
