a51745f853bbd27a3faf995ff7b0783d0a94e3a0
Mafrans <malte.kluft@gmail.com>
2021-02-11 08:34:06 +0100

    Designa Item-Editorn

---
+++ src/main/kotlin/me/mafrans/soloadventure/editor/ItemEditor.java
@@ -0,0 +1,17 @@
+package me.mafrans.soloadventure.editor;
+
+import javax.swing.*;
+
+public class ItemEditor {
+    private JButton saveButton;
+    private JTextField nameField;
+    private JTextArea descriptionArea;
+    private JTabbedPane tabbedPane1;
+    private JCheckBox isWeaponCheckBox;
+    private JList messageList;
+    private JSpinner attackDamageSpinner;
+    private JComboBox colorComboBox;
+    private JButton newAttackMessageButton;
+    private JList tagList;
+    private JButton newTagButton;
+}
